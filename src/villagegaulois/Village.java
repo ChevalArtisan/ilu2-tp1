71f737a395c5082ad679bac592329c11bf5b27be
Hippolyte Handwerk <hippolyte.handwerk@gmail.com>
2023-02-15 10:16:34 +0100

    Debut TP1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -13,7 +13,9 @@ public class Village {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
 	}
-
+	private class Marche{
+		private Etal[] etals;
+	}
 	public String getNom() {
 		return nom;
 	}
